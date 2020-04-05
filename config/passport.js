1ae67966be49588ba8ba649b956f10e8aac1b69b
Jerry Lai <jerrydavid123@hotmail.com>
2020-04-05 10:58:01 +0800

    finish facebook thirdparty login

--- config/passport.js
+++ config/passport.js
@@ -34,41 +34,45 @@ module.exports = passport => {
   )
 
   passport.use(
-    new FacebookStrategy({
-      clientID: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-      callbackURL: process.env.FACEBOOK_CALLBACK,
-      profileFields: ['email', 'displayName']
-    }, (accessToken, refreshToken, profile, done) => {
-      // find and create user
-      User.findOne({
-        where: {
-          email: profile._json.email
-        }
-      }).then(user => {
-        // 如果 email 不存在就建立新的使用者
-        if (!user) {
-          // 隨機建立密碼
-          let randomPassword = Math.random().toString(36).slice(-8)
-          bcrypt.genSalt(10, (err, salt) =>
-            bcrypt.hash(randomPassword, salt, (err, hash) => {
-              let newUser = User({
-                name: profile._json.name,
-                email: profile._json.email,
-                password: hash
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName'],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // find and create user
+        User.findOne({
+          email: profile._json.email,
+        }).then(user => {
+          if (!user) {
+            var randomPassword = Math.random()
+              .toString(36)
+              .slice(-8)
+            bcrypt.genSalt(10, (err, salt) =>
+              bcrypt.hash(randomPassword, salt, (err, hash) => {
+                var newUser = new User({
+                  name: profile._json.name,
+                  email: profile._json.email,
+                  password: hash,
+                })
+                newUser
+                  .save()
+                  .then(user => {
+                    return done(null, user)
+                  })
+                  .catch(err => {
+                    console.log(err)
+                  })
               })
-              newUser.save().then(user => {
-                return done(null, user)
-              }).catch(err => {
-                console.log(err)
-              })
-            })
-          )
-        } else {
-          return done(null, user)
-        }
-      })
-    })
+            )
+          } else {
+            return done(null, user)
+          }
+        })
+      }
+    )
   )
 
   passport.serializeUser((user, done) => {
